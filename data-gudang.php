2e6cd48bedb7bf7b852b99566d7f12fab016743b
Arief <arief.d2202@gmail.com>
2024-03-06 15:57:13 +0700

    fix logout

--- data-gudang.php
+++ data-gudang.php
@@ -9,7 +9,7 @@
 
     if(isset($_POST['logout'])){
         $_SESSION['username'] = null;
-        header("Location: /login.php");
+        header("Location: /");
         exit();
     }
     if(isset($_POST['delete'])){
