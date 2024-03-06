6a037b6e5a753b666bb96d67e59902ea843fbef6
Arief <arief.d2202@gmail.com>
2024-03-06 22:50:09 +0700

    fix bug

--- check_card_kasir.php
+++ check_card_kasir.php
@@ -7,15 +7,9 @@
         $resultSearchCard = mysqli_fetch_object(mysqli_query($koneksi, $sqlSearchCard));
         $sqlSearchCard2 = "SELECT * FROM card WHERE card=\"".$card."\";";
         $resultSearchCard2 = mysqli_fetch_object(mysqli_query($koneksi, $sqlSearchCard2));
-        
-
-        $sqlSearchCardPenjualan = "SELECT * FROM data_penjualan WHERE card=\"".$card."\" AND checked_out = 0;";
-        $resultSearchCardPenjualan = mysqli_fetch_object(mysqli_query($koneksi, $sqlSearchCardPenjualan));
-        
+                
         $sql = "SELECT * FROM data_apotek WHERE card=\"scan\";";
         $result = mysqli_fetch_object(mysqli_query($koneksi, $sql));
-        $sqlPenjualan = "SELECT * FROM data_penjualan WHERE card=\"scan\";";
-        $resultPenjualan = mysqli_fetch_object(mysqli_query($koneksi, $sqlPenjualan));
         if($result){
             if($resultSearchCard){
                 echo json_encode([
@@ -40,30 +34,6 @@
                 ]);
             }
         }
-        else if($resultPenjualan){
-            if($resultSearchCardPenjualan){
-                echo json_encode([
-                    'status' => 'failed',
-                    'pesan' => "Gagal input obat ke penjualan, kartu sudah ada di keranjang penjualan"
-                ]);
-
-            }
-            else if (!$resultSearchCard2){
-                echo json_encode([
-                    'status' => 'failed',
-                    'pesan' => "Gagal input kartu ke data baru, Kartu tidak ditemukan di database gudang"
-                ]);
-            }
-            else{
-                $sql = "UPDATE `data_penjualan` SET `card` = '".$card."' WHERE `data_penjualan`.`id` = ".$resultPenjualan->id.";";
-                $result = mysqli_query($koneksi, $sql);
-                
-                echo json_encode([
-                    'status' => 'success',
-                    'pesan' => "Berhasil Menambahkan Obat di Penjualan"
-                ]);
-            }
-        }
         else{
             if(!$resultSearchCard){
                 echo json_encode([
@@ -71,14 +41,45 @@
                     'pesan' => "Kartu tidak ditemukan di database apotek"
                 ]);
             } else 
-            if($resultSearchCard2 && $resultSearchCard2->obat_id != 0){                
-                $sqlSearchObat = "SELECT * FROM data_gudang WHERE id=\"".$resultSearchCard2->obat_id."\";";
-                $resultSearchObat = mysqli_fetch_object(mysqli_query($koneksi, $sqlSearchObat));
-                echo json_encode([
-                    'status' => 'success',
-                    'card' => $resultSearchCard2,
-                    'obat' => $resultSearchObat
-                ]);
+            if($resultSearchCard2 && $resultSearchCard2->obat_id != 0){                        
+                $sqlPenjualan = "SELECT * FROM data_penjualan WHERE card=\"scan\";";
+                $resultPenjualan = mysqli_fetch_object(mysqli_query($koneksi, $sqlPenjualan));
+                
+                if($resultPenjualan){
+                    $sqlSearchCardPenjualan = "SELECT * FROM data_penjualan WHERE card=\"".$card."\" AND checked_out = 0;";
+                    $resultSearchCardPenjualan = mysqli_fetch_object(mysqli_query($koneksi, $sqlSearchCardPenjualan));
+                    if($resultSearchCardPenjualan){
+                        echo json_encode([
+                            'status' => 'failed',
+                            'pesan' => "Gagal input obat ke penjualan, kartu sudah ada di keranjang penjualan"
+                        ]);
+
+                    }
+                    else if (!$resultSearchCard2){
+                        echo json_encode([
+                            'status' => 'failed',
+                            'pesan' => "Gagal input kartu ke data baru, Kartu tidak ditemukan di database gudang"
+                        ]);
+                    }
+                    else{
+                        $sql = "UPDATE `data_penjualan` SET `card` = '".$card."' WHERE `data_penjualan`.`id` = ".$resultPenjualan->id.";";
+                        $result = mysqli_query($koneksi, $sql);
+                        
+                        echo json_encode([
+                            'status' => 'success',
+                            'pesan' => "Berhasil Menambahkan Obat di Penjualan"
+                        ]);
+                    }
+                }
+                else{
+                    $sqlSearchObat = "SELECT * FROM data_gudang WHERE id=\"".$resultSearchCard2->obat_id."\";";
+                    $resultSearchObat = mysqli_fetch_object(mysqli_query($koneksi, $sqlSearchObat));
+                    echo json_encode([
+                        'status' => 'success',
+                        'card' => $resultSearchCard2,
+                        'obat' => $resultSearchObat
+                    ]);
+                }
             }
             else{
                 echo json_encode([
